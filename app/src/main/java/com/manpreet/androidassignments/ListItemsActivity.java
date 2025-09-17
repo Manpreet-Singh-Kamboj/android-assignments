5ab006ef08b459d0756012d2bb853e4544cd1473
Manpreet-Singh-Kamboj <manpreetkamboj60.net@gmail.com>
2025-09-17 17:06:51 -0400

    Added 3 Different Activites with Navigation

--- app/src/main/java/com/manpreet/androidassignments/ListItemsActivity.java
+++ app/src/main/java/com/manpreet/androidassignments/ListItemsActivity.java
@@ -1,37 +1,49 @@
 package com.manpreet.androidassignments;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import androidx.appcompat.widget.Toolbar;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
 import androidx.core.app.NavUtils;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+
+public class ListItemsActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_list_items);
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        }
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         if(getSupportActionBar() != null){
             getSupportActionBar().setDisplayHomeAsUpEnabled(true);
         }
+        Button navigateToMainActivityButton = findViewById(R.id.navigate_to_main_activity);
+        navigateToMainActivityButton.setOnClickListener(this::onNavigateToMainActivityClick);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
     }
-
+    public void onNavigateToMainActivityClick(View view){
+        Intent intent = new Intent(this,MainActivity.class);
+        startActivity(intent);
+    }
     @Override
-    public boolean onOptionsItemSelected(MenuItem item){
+    public boolean onOptionsItemSelected(MenuItem item) {
         if(item.getItemId() == android.R.id.home){
             NavUtils.navigateUpFromSameTask(this);
             return true;
--- app/src/main/java/com/manpreet/androidassignments/LoginActivity.java
+++ app/src/main/java/com/manpreet/androidassignments/LoginActivity.java
@@ -1,28 +1,30 @@
 package com.manpreet.androidassignments;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.core.app.NavUtils;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        if(getSupportActionBar() != null){
-            getSupportActionBar().setDisplayHomeAsUpEnabled(true);
-        }
+        Button loginButton = findViewById(R.id.login_button);
+        loginButton.setOnClickListener(this::onLoginClick);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
@@ -30,12 +32,15 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item){
-        if(item.getItemId() == android.R.id.home){
-            NavUtils.navigateUpFromSameTask(this);
-            return true;
+    private void onLoginClick(View view){
+        Intent intent = new Intent(this,ListItemsActivity.class);
+        EditText loginInput = findViewById(R.id.email);
+        EditText passwordInput = findViewById(R.id.password);
+        if(loginInput.getText().length() == 0 || passwordInput.getText().length() == 0){
+            Toast inputValidation = Toast.makeText(this,"Email or Password cannot be empty", Toast.LENGTH_LONG);
+            inputValidation.show();
+            return;
         }
-        return super.onOptionsItemSelected(item);
+        startActivity(intent);
     }
 }
